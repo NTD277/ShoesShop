6dc975ec432898fcbddc941d527ccfde90c3f21a
NTD277 <ntd270799@gmail.com>
2020-11-23 23:47:18 +0700

    up cuối của cuối 2

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -10,11 +10,16 @@ class CartController extends Controller
 {
     public function index(Request $request)
     {
+        if (session('idCustommer')) {
         $idProduct = $request->get('id');
         $infoProduct= DB::table('products')->select('products.*')
             ->where('id' , '=',$idProduct)->get();
         $request->session()->put('cart', $infoProduct);
 //        session(['cart' => $infoProduct]);
         return view('frontend.cart.index');
+        }else{
+            $request->session()->flash('mess','Đăng nhập để mua hàng');
+            return redirect(route('fr.login'));
+        }
     }
 }
--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -9,34 +9,37 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends BaseController
 {
-    public function index(Product $product, $slug)
+    public function index(Request $request,Product $product, $slug)
     {
-        $title = 'Chi tiết';
-        $newProduct = DB::table('products')
-            ->where('status', 1)
-            ->where('slug', $slug)
-            ->get();
-        $colorProduct = DB::table('products')
-            ->join('productproperties', 'productproperties.idProduct', '=', 'products.id')
-            ->join('properties', 'properties.id', '=', 'productproperties.idProperty')
-            ->where('slug', $slug)
-            ->where('properties.name', '=', 'màu')
-            ->get();
-        $sizeProduct = DB::table('products')
-            ->join('productproperties', 'productproperties.idProduct', '=', 'products.id')
-            ->join('properties', 'properties.id', '=', 'productproperties.idProperty')
-            ->where('slug', $slug)
-            ->where('properties.name', '=', 'size')
-            ->get();
-        $imageProduct = DB::table('image_products')
-            ->select('image_products.*')
-            ->join('products', 'image_products.idProduct', '=','products.id')
-            ->where('products.slug', $slug)
-            ->get();
-//        dd($imageProduct);
-//        $newProductHeader = $product->SelectByNumber(5,'adidas');
-        return view('frontend.products.show', compact('title','newProduct', 'colorProduct', 'sizeProduct','imageProduct'));
+
+            $title = 'Chi tiết';
+            $newProduct = DB::table('products')
+                ->where('status', 1)
+                ->where('slug', $slug)
+                ->get();
+            $colorProduct = DB::table('products')
+                ->join('productproperties', 'productproperties.idProduct', '=', 'products.id')
+                ->join('properties', 'properties.id', '=', 'productproperties.idProperty')
+                ->where('slug', $slug)
+                ->where('properties.name', '=', 'màu')
+                ->get();
+            $sizeProduct = DB::table('products')
+                ->join('productproperties', 'productproperties.idProduct', '=', 'products.id')
+                ->join('properties', 'properties.id', '=', 'productproperties.idProperty')
+                ->where('slug', $slug)
+                ->where('properties.name', '=', 'size')
+                ->get();
+            $imageProduct = DB::table('image_products')
+                ->select('image_products.*')
+                ->join('products', 'image_products.idProduct', '=', 'products.id')
+                ->where('products.slug', $slug)
+                ->get();
+            //        dd($imageProduct);
+            //        $newProductHeader = $product->SelectByNumber(5,'adidas');
+            return view('frontend.products.show', compact('title', 'newProduct', 'colorProduct', 'sizeProduct', 'imageProduct'));
+
     }
+
 
     public function ProductBrand()
     {
